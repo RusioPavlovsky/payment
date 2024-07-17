ad7f49f0f29f3c8f90b7608ae4ac6336c6990576
Ruslan Pavlov <rpavlov@ubic.tech>
2024-07-17 18:51:16 +0300

    initial commit

---
+++ src/test/java/rusio/pavlovsky/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package rusio.pavlovsky.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
